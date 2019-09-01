6ed6ed3d40165eab1532b65b70ca8a9e5d3f2ccd
rdamri <amry.darmawikarta@gmail.com>
2019-09-01 13:43:45 -0700

    Update prepare.py

--- Mal-Engine-Core/prepare.py
+++ Mal-Engine-Core/prepare.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 import encoder
 
-# generate an attribute.names file to be used with `ergo relevance <path> --attributes attributes.names`
+# generate attribute.names 
 def prepare_names():
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "attribute.names")
     log.info("creating %s" % path)
@@ -18,22 +18,21 @@ def prepare_names():
         for name in encoder.attribute_names():
             fp.write("%s\n" % name)
 
-# generate a classes.json file 
+# generate classes
 def prepare_classes():
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "classes.json")
     log.info("creating %s" % path)
     with open(path, 'w+t') as fp:
         json.dump(encoder.classes, fp)
 
-# called by `ergo train <path> --dataset file.csv` to parse the dataset.
+# trainning dataset
 def prepare_dataset(filename):
     prepare_names()
     prepare_classes()
     data = pd.read_csv(filename, sep = ',', header = None)
     return data.replace(encoder.classes_replace)
 
-# used by `ergo encode <path> <folder>` to encode a PE in a vector of scalar features
-# used by `ergo serve <path>` to parse the input query before running the inference
+# templatenya ergo encode <path> <folder>
 def prepare_input(x, is_encoding = False):
     # file upload
     if isinstance(x, werkzeug.datastructures.FileStorage):
