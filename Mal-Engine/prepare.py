25ebb63865ecce69613f01afc9c0d1b627199664
rdamri <amry.darmawikarta@gmail.com>
2019-09-01 12:49:20 -0700

    Add files via upload

---
+++ Mal-Engine/prepare.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import json
+import logging as log
+import numpy as np
+import pandas as pd
+import werkzeug.datastructures
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+import encoder
+
+# generate a classes.json file 
+def prepare_classes():
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "classes.json")
+    log.info("creating %s" % path)
+    with open(path, 'w+t') as fp:
+        json.dump(encoder.classes, fp)
+
+#buat training dataset.csv
+def prepare_dataset(filename):
+    prepare_names()
+    prepare_classes()
+    data = pd.read_csv(filename, sep = ',', header = None)
+    return data.replace(encoder.classes_replace)
+
+# templatenya ergo encode <path> <folder>
+def prepare_input(x, is_encoding = False):
+    # buat upload file
+    if isinstance(x, werkzeug.datastructures.FileStorage):
+        return encoder.encode_pe(x)
+    # deklarasi file path yg mau di encode
+    elif os.path.isfile(x) :
+        return encoder.encode_pe(x)
+    # pemisah
+    else:
+        return x.split(',')
+
+
---
+++ Mal-Engine/train.py
@@ -0,0 +1,32 @@
+import os
+import logging as log
+
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+#training modelnya
+def train_model(model, dataset):
+    log.info("training model (train on %d samples, validate on %d) ..." % ( \
+            len(dataset.Y_train), 
+            len(dataset.Y_val) ) )
+    
+    loss      = 'binary_crossentropy'
+    optimizer = 'adam'
+    metrics   = ['accuracy']
+    
+    model.compile(loss = loss, optimizer = optimizer, metrics = metrics)
+
+    checkpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "checkpoints")
+    os.makedirs(checkpath, exist_ok=True)
+    checkpath = os.path.join(checkpath, 'model-epoch{epoch:03d}-acc{val_acc:03f}.h5')
+
+    # buat berentiin proses trainin kalo val_acc ya ga ada perubahan
+    stopper = EarlyStopping(monitor = 'val_acc', min_delta=0.0001, patience = 5, mode = 'auto')
+    # buat men-generate hasil training
+    saver = ModelCheckpoint(checkpath, save_best_only=True, verbose=1, monitor='val_loss', mode='min')
+    # start training
+    return model.fit( dataset.X_train, dataset.Y_train,
+            batch_size = 64,
+            epochs = 5,
+            verbose = 2,
+            validation_data = (dataset.X_val, dataset.Y_val),
+            callbacks = [saver, stopper])
