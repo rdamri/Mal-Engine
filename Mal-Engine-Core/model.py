0fcc768769ae9f322d3e0850d34268593f82d1b1
rdamri <amry.darmawikarta@gmail.com>
2019-09-01 13:40:56 -0700

    Update model.py

--- Mal-Engine-Core/model.py
+++ Mal-Engine-Core/model.py
@@ -3,11 +3,9 @@ import logging as log
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 
-# used by `ergo train <path>` to build the model structure
+# train model
 def build_model(is_train):  
-    # our model is a simple ANN with 486 inputs, two hidden
-    # layers of 70 neurons each, dropout of 30% while training
-    # and two output classes (0=clean 1=malicious)
+
     n_inputs = 486
     log.info("building model for %s ..." % 'training' if is_train else 'evaluation')
     if is_train:
